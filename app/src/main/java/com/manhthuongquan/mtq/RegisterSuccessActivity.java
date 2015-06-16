c61066c420fe10a8a75d896bb51d48edbf401872
toannb <toannb@peacesoft.net>
2015-06-16 23:36:34 +0700

    Rejected commit: Default

---
+++ app/src/main/java/com/manhthuongquan/mtq/RegisterSuccessActivity.java
@@ -0,0 +1,15 @@
+package com.manhthuongquan.mtq;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ToanNB on 6/16/2015.
+ */
+public class RegisterSuccessActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_success);
+    }
+}
--- app/src/main/java/fragment/RegisterFragmentStep3.java
+++ app/src/main/java/fragment/RegisterFragmentStep3.java
@@ -1,6 +1,6 @@
 package fragment;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,18 +8,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.manhthuongquan.mtq.R;
+import com.manhthuongquan.mtq.RegisterSuccessActivity;
 
 /**
  * Created by ToanNB on 6/11/2015.
  */
 public class RegisterFragmentStep3 extends Fragment {
 
+    private Button mCompleteButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.register_step3_layout, container, false);
+        View view = inflater.inflate(R.layout.register_step3_layout, container, false);
+
+        mCompleteButton = (Button)view.findViewById(R.id.register_step3_complete_button_id);
+        return view;
     }
 
     @Override
@@ -27,13 +34,12 @@ public class RegisterFragmentStep3 extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         ((ActionBarActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        mCompleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplicationContext(), RegisterSuccessActivity.class));
+            }
+        });
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-//        ActionBar actionBar = ((ActionBarActivity)getActivity()).getSupportActionBar();
-//        actionBar.setHomeButtonEnabled(true);
-//        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
