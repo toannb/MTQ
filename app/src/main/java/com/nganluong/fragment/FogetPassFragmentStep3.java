20b5489a113c8bae10cfe52ff9863a538cdcb9ad
DucChinh <chinh.duc.169@gmail.com>
2015-06-18 17:00:20 +0700

    commit

--- app/src/main/java/com/nganluong/fragment/FogetPassFragmentStep3.java
+++ app/src/main/java/com/nganluong/fragment/FogetPassFragmentStep3.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.nganluong.config.Variables;
@@ -14,9 +15,10 @@ import com.nganluong.manhthuongquan.R;
 /**
  * Created by DucChinh on 6/18/2015.
  */
-public class FogetPassFragmentStep3 extends Fragment {
+public class FogetPassFragmentStep3 extends Fragment implements View.OnClickListener {
 
-    private TextView txtBuoc3, txtTitleBuoc3;
+    private TextView txtBuoc3, txtTitleBuoc3, txtConfirmChangePass;
+    private EditText editPassNew, editConfirmPassNew;
 
     @Nullable
     @Override
@@ -25,10 +27,28 @@ public class FogetPassFragmentStep3 extends Fragment {
 
         txtBuoc3 = (TextView) view.findViewById(R.id.txtBuoc3);
         txtTitleBuoc3 = (TextView) view.findViewById(R.id.txtTitleBuoc3);
+        editPassNew = (EditText) view.findViewById(R.id.editPassNew);
+        editConfirmPassNew = (EditText) view.findViewById(R.id.editConfirmPassNew);
+        txtConfirmChangePass = (TextView) view.findViewById(R.id.txtConfirmChangePass);
 
         txtBuoc3.setTypeface(Variables.objFont);
         txtTitleBuoc3.setTypeface(Variables.objFont);
+        editPassNew.setTypeface(Variables.objFont);
+        editConfirmPassNew.setTypeface(Variables.objFont);
+        txtConfirmChangePass.setTypeface(Variables.objFont);
+
+        txtConfirmChangePass.setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.txtConfirmChangePass:
+
+                break;
+        }
     }
 }
