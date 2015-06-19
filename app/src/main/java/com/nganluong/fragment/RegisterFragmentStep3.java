c87de2f78de310d297fd3c2ef825f8531eb2067d
toannb <toannb@peacesoft.net>
2015-06-19 08:51:08 +0700

    lưới trang chủ

--- app/src/main/java/com/nganluong/fragment/RegisterFragmentStep3.java
+++ app/src/main/java/com/nganluong/fragment/RegisterFragmentStep3.java
@@ -3,7 +3,6 @@ package com.nganluong.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
